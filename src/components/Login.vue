f05110ebcfa8c08f158d3cff5c864574250bcf7e
salvo <salvolupo.g@gmail.com>
2018-02-22 02:41:53 +0100

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="container">
+    <div class="row justify-content-center">
+
+        <div class="card border-primary mb-3" >
+            <div class="card-header">Login</div>
+            <div class="card-body text-primary">
+                <form>
+                    <div class="form-group">
+                        <input type="text" v-model="username" class="form-control" placeholder="Username">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" v-model="password" class="form-control" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                        <button class="btn btn-raised btn-primary" v-on:click="doLogin">Login</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+        
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  methods: {
+    doLogin (): void {
+      alert('ok!')
+    }
+  }
+})
+</script>
+
+
